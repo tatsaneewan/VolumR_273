eb21ce8f6fe1e3dc6301d525401fd285dffc5313
Supachai Ghudkam <56030342@kmitl.ac.th>
2017-05-05 13:07:14 +0700

    R
    Update

---
+++ R.ino
@@ -0,0 +1,13 @@
+int ledPin=3;
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(3,OUTPUT);
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+  analogWrite(ledPin, sensorValue);
+  delay(10);
+}
